88222800d261b2dd88c52b3914a16dff66cdcc43
Julian Peeters <julianpeeters@gmail.com>
2012-08-23 01:43:23 -0700

    salat-avro is reading a case class and printing an avro

--- build.sbt
+++ build.sbt
@@ -4,17 +4,23 @@ version := "0.0.1-SNAPSHOT"
 
 organization := "com.julianpeeters"
 
+//scalaVersion := "2.8.1"
 scalaVersion := "2.9.1"
 
-resolvers ++= Seq(
-  "repo.scalatools rels" at "https://oss.sonatype.org/content/groups/scala-tools/",
-  "repo.scalatools snaps" at "https://oss.sonatype.org/content/repositories/snapshots/"
-)
+resolvers += "repo.scalatools rels" at "https://oss.sonatype.org/content/groups/scala-tools/"
+
+resolvers += "repo.scalatools snaps" at "https://oss.sonatype.org/content/repositories/snapshots/"
+
+resolvers += "repo.novus rels" at "http://repo.novus.com/releases/"
+
+resolvers += "repo.novus snaps" at "http://repo.novus.com/snapshots/"
+
+resolvers += Resolver.file("Local Ivy Repository", file("/home/julianpeeters/.ivy2/local/"))(Resolver.ivyStylePatterns)
 
 libraryDependencies ++= Seq( 
   "org.scalatest" %% "scalatest" % "1.8" % "test", 
-  "org.apache.avro" % "avro" % "1.5.1",
-  "com.novus" %% "salat-core" % "1.9.0", 
-  "ch.qos.logback" % "logback-core" % "1.0.0",
-  "ch.qos.logback" % "logback-classic" % "1.0.0"  
+  "org.apache.avro" % "avro" % "1.6.2",
+  "com.banno.salat.avro" %% "salat-avro" % "0.0.7.2-SNAPSHOT"
+// ,"ch.qos.logback" % "logback-core" % "1.0.0",
+// "ch.qos.logback" % "logback-classic" % "1.0.0"  
 )
--- src/main/scala/salat-avro-example.scala
+++ src/main/scala/salat-avro-example.scala
@@ -1,18 +1,45 @@
 import models._
-import com.novus.salat._
-import com.novus.salat.annotations._
-import com.novus.salat.global._
+
+import com.banno.salat.avro._
+
+//import com.banno.salat.avro.AvroGrater._
+import global._
+import java.io.ByteArrayOutputStream
+import org.apache.avro._
+import org.apache.avro.generic._
+import org.apache.avro.io._
 
 object Main extends Application {
   val greeting = "Hello World!" 
     Console.println(greeting) 
+  
+  val baos = new ByteArrayOutputStream
+  val binaryEncoder = EncoderFactory.get().binaryEncoder(baos, null)
 
   val a = Record(greeting)
     Console.println(a)
 
-  val dbo = grater[Record].asDBObject(a)
+  val dbo = grater[Record].serialize(a, binaryEncoder)
     Console.println(dbo)
+  
+ // val schema = Schema.parse(getClass().getResourceAsStream("schema_record.avsc"))
+   // Console.println(schema)
+
+  val schema: Schema = grater[Record].asAvroSchema
+    Console.println(schema)
+
+ // val gr = new GenericData.Record(schema) 
+  //  Console.println(gr)
+
+  //val gdw = new GenericDatumWriter(schema)
+   // Console.println(gdw)
+   // gdw.write(gr, binaryEncoder)
+
+
+ // val output = gdw.write(dbo, binaryEncoder)
+   // Console.println(output)
+
+ // baos.toByteArray() //1st step for decoding?
 
 }
-
 
